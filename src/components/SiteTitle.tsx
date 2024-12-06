5765e5e6949a383667582461f3198e7d7254cb71
Luke N. Arnold <lukenarnold@gmail.com>
2024-12-05 20:06:45 -0800

    link icon (keyboard overwrite awareness)

--- src/components/SiteTitle.tsx
+++ src/components/SiteTitle.tsx
@@ -29,8 +29,24 @@ export default function SiteTitle() {
         fontSize: '14px',
         zIndex: 1000,
         cursor: 'pointer',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px'
       }}>
       {copied ? 'url copied!' : 'polyp.observer'}
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+        <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+      </svg>
     </div>
   );
 };
